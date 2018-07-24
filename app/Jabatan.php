b3b31189289bc5ce4ba607dfa6245e400c15d36c
hudaparodi <hudaparodi@gmail.com>
2018-07-24 10:42:16 +0700

    2-data-jabatan add sub divisi

--- app/Jabatan.php
+++ app/Jabatan.php
@@ -9,6 +9,6 @@ class Jabatan extends Model
     protected $table = 'm_jabatan' ;
 
     protected $fillable = [
-        'c_posisi','c_id','c_divisi_id'
+        'c_posisi','c_id','c_divisi_id','c_sub_divisi_id'
     ];
 }
