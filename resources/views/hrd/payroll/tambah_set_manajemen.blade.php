502f9ed94e278f2f5eac71e520021921703af0eb
hudaparodi <hudaparodi@gmail.com>
2018-07-24 20:24:56 +0700

    12-maping-gaji-manajemen view setting gaji manajemen

---
+++ resources/views/hrd/payroll/tambah_set_manajemen.blade.php
@@ -0,0 +1,161 @@
+@extends('main') @section('content')
+<!--BEGIN PAGE WRAPPER-->
+<div id="page-wrapper">
+  <!--BEGIN TITLE & BREADCRUMB PAGE-->
+  <div id="title-breadcrumb-option-demo" class="page-title-breadcrumb">
+    <div class="page-header pull-left" style="font-family: 'Raleway', sans-serif;">
+      <div class="page-title">Payroll</div>
+    </div>
+    <ol class="breadcrumb page-breadcrumb pull-right" style="font-family: 'Raleway', sans-serif;">
+      <li>
+        <i class="fa fa-home"></i>&nbsp;
+        <a href="{{ url('/home') }}">Home</a>&nbsp;&nbsp;
+        <i class="fa fa-angle-right"></i>&nbsp;&nbsp;</li>
+      <li>
+        <i></i>&nbsp;HRD&nbsp;&nbsp;
+        <i class="fa fa-angle-right"></i>&nbsp;&nbsp;</li>
+      <li class="active">Setting Gaji</li>
+    </ol>
+    <div class="clearfix">
+    </div>
+  </div>
+  <div class="page-content fadeInRight">
+    <div id="tab-general">
+      <div class="row mbl">
+        <div class="col-lg-12">
+          <div class="col-md-12">
+            <div id="area-chart-spline" style="width: 100%; height: 300px; display: none;">
+            </div>
+          </div>
+          <ul id="generalTab" class="nav nav-tabs">
+            <li class="active">
+              <a href="#alert-tab" data-toggle="tab">Setting Gaji</a>
+            </li>
+            <!-- <li><a href="#note-tab" data-toggle="tab">2</a></li>
+                            <li><a href="#label-badge-tab-tab" data-toggle="tab">3</a></li> -->
+          </ul>
+          <div id="generalTabContent" class="tab-content responsive">
+            <div id="alert-tab" class="tab-pane fade in active">
+              <div class="row">
+                <div class="col-md-12">
+                  <form method="POST" action="{{ url('hrd/payroll/simpan-gaji-man') }}">
+                    {{ csrf_field() }}
+                    <div class="col-md-12 col-sm-12 col-xs-12" style="margin-bottom: 20px; padding-bottom:5px;padding-top:15px;padding-left:-10px;padding-right: -10px; ">
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">Nama Gaji</label>
+                      </div>
+                      <div class="col-md-4 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <input type="text" name="nm_gaji" class="form-control input-sm">
+                        </div>
+                      </div>
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">Jumlah </label>
+                      </div>
+                      <div class="col-md-4 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <div class="input-group">
+                            <input type="number" id="jumlah" name="jumlah" class="form-control input-sm">
+                            <span class="input-group-btn">
+                              <button type="button" class="btn btn-success btn-sm" onclick="samakan()">Samakan</button>
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">Jabatan</label>
+                      </div>
+                      <div class="col-md-4 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <select id="" name="c_jabatan" class="form-control input-sm">
+                            <option>--pilih jabatan--</option>
+                            <option value="1">Leader</option>
+                            <option value="2">Staf</option>
+                            <option value="3">Semua</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">SD</label>
+                      </div>
+                      <div class="col-md-4 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <input type="number" id="sd" name="c_sd" class="form-control input-sm">
+                        </div>
+                      </div>
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">SMP</label>
+                      </div>
+                      <div class="col-md-4 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <input type="number" id="smp" name="c_smp" class="form-control input-sm">
+                        </div>
+                      </div>
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">SMA</label>
+                      </div>
+                      <div class="col-md-4 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <input type="number" id="sma" name="c_sma" class="form-control input-sm">
+                        </div>
+                      </div>
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">D1</label>
+                      </div>
+                      <div class="col-md-4 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <input type="number" id="d1" name="c_d1" class="form-control input-sm">
+                        </div>
+                      </div>
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">D2</label>
+                      </div>
+                      <div class="col-md-4 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <input type="number" id="d2" name="c_d2" class="form-control input-sm">
+                        </div>
+                      </div>
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">D3</label>
+                      </div>
+                      <div class="col-md-4 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <input type="number" id="d3" name="c_d3" class="form-control input-sm">
+                        </div>
+                      </div>
+                      <div class="col-md-2 col-sm-4 col-xs-12">
+                        <label class="tebal">S1</label>
+                      </div>
+                      <div class="col-md-4 col-sm-8 col-xs-12">
+                        <div class="form-group">
+                          <input type="number" id="s1" name="c_s1" class="form-control input-sm">
+                        </div>
+                      </div>
+                      <div class="col-md-6 col-sm-12 col-xs-12">
+                        <input type="button" value="Batal" class="btn btn-danger btn-block">
+                      </div>
+                      <div class="col-md-6 col-sm-12 col-xs-12">
+                        <input type="submit" value="Simpan" class="btn btn-primary btn-block">
+                      </div>
+                  </form>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          @endsection @section('extra_scripts')
+          <script type="text/javascript">
+            function samakan() {
+              var jum = $('#jumlah').val();
+              console.log(jum);
+              $('#sd').val(jum);
+              $('#smp').val(jum);
+              $('#sma').val(jum);
+              $('#d1').val(jum);
+              $('#d2').val(jum);
+              $('#d3').val(jum);
+              $('#s1').val(jum);
+            }
+            
+          </script> @endsection
